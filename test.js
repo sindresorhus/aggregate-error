140799656e4f328b8c8b6895a1979ff8d9e00eac
Felix Becker <felix.b@outlook.com>
2017-12-21 12:16:46 -0800

    Support plain error objects with message property

--- test.js
+++ test.js
@@ -2,9 +2,19 @@ import test from 'ava';
 import AggregateError from './';
 
 test(t => {
-	const err = new AggregateError([new Error('foo'), 'bar']);
+	const err = new AggregateError([
+		new Error('foo'),
+		'bar',
+		{message: 'baz', code: 'EBAZ'},
+		{code: 'EQUX'}
+	]);
 	console.log(err);
 	t.regex(err.message, /Error: foo\n {8}at /);
 	t.regex(err.message, /Error: bar\n {8}at /);
-	t.deepEqual(Array.from(err), [new Error('foo'), new Error('bar')]);
+	t.deepEqual(Array.from(err), [
+		new Error('foo'),
+		new Error('bar'),
+		Object.assign(new Error('baz'), {code: 'EBAZ'}),
+		Object.assign(new Error(), {code: 'EQUX'})
+	]);
 });
