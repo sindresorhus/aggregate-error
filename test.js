a0c122bf034f004a5d107749404bff5ad477c0bd
Konrad Mohrfeldt <konrad.mohrfeldt@farbdev.org>
2019-02-28 12:18:04 +0100

    Gracefully handle `Error` instances without a `stack` property (#7)

--- test.js
+++ test.js
@@ -26,3 +26,28 @@ test('main', t => {
 		Object.assign(new Error(), {code: 'EQUX'})
 	]);
 });
+
+test('gracefully handle Error instances without a stack', t => {
+	class StacklessError extends Error {
+		constructor(...args) {
+			super(...args);
+			this.name = this.constructor.name;
+			delete this.stack;
+		}
+	}
+
+	const error = new AggregateError([
+		new Error('foo'),
+		new StacklessError('stackless')
+	]);
+
+	console.log(error);
+
+	t.regex(error.message, /Error: foo\n {8}at /);
+	t.regex(error.message, /StacklessError: stackless/);
+
+	t.deepEqual([...error], [
+		new Error('foo'),
+		new StacklessError('stackless')
+	]);
+});
