8ba26ecab64de4216905aded38ef2befae6d850b
Gregor Martynus <gregor@martynus.net>
2020-08-21 15:27:57 -0700

    Support custom `Error` subclass in the TypeScript types (#13)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -12,3 +12,20 @@ expectType<IterableIterator<Error>>(aggregateError[Symbol.iterator]());
 for (const error of aggregateError) {
 	expectType<Error>(error);
 }
+
+class CustomError extends Error {
+	public foo: string;
+
+	constructor(message: string) {
+		super(message)
+		this.name = 'CustomError';
+		this.foo = 'bar';
+	}
+}
+const customAggregateError = new AggregateError<CustomError>([
+	new CustomError('foo')
+]);
+
+for (const error of customAggregateError) {
+	expectType<string>(error.foo);
+}
